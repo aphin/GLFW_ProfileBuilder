e0e77f2bd7406e5ae5f98d6277c88526b5b03c34
aphin <vivalzard@mail.ru>
2014-05-23 13:29:23 +0400

    Update

---
+++ profb.c
@@ -0,0 +1,12 @@
+#include <GLFW/glfw3.h>		// Need GLFW 3.0 or newer. Change library name if it is wrong
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "profb.h"			// Header file
+
+int main (int argc, char *argv[])	// MAIN
+{
+	return 0;
+}
